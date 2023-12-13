206a38100312fe332d066961fd8db9cc78f18cee
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-13 13:51:06 -0500

    lesson

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+router.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+router.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+router.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,68 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+/*
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+
+router.get('/', (req, res) => {
+  return res.send(Object.values(users));
 });
+
+router.get('/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+router.post('/', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+router.put('/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+router.delete('/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+router.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+router.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
 
 module.exports = router;
